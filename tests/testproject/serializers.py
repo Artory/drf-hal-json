5a474b5dc278aab7456010988c074a67050e1037
Johannes Staffans <johannes.staffans@artory.com>
2017-05-22 15:37:06 +0200

    include field definitions

--- tests/testproject/serializers.py
+++ tests/testproject/serializers.py
@@ -5,15 +5,19 @@ from .models import TestResource, RelatedResource2, RelatedResource1
 class TestResourceSerializer(HalModelSerializer):
     class Meta:
         model = TestResource
-        fields = ('self', 'id', 'name', 'related_resource_1')
+        fields = ('self', 'id', 'name', 'related_resource_1', 'related_resource_2')
         depth = 1
 
 
 class RelatedResource1Serializer(HalModelSerializer):
     class Meta:
         model = RelatedResource1
+        fields = ('self', 'name', 'active')
+        depth = 1
 
 
 class RelatedResource2Serializer(HalModelSerializer):
     class Meta:
         model = RelatedResource2
+        fields = ('self', 'name', 'active', 'related_resources_1')
+        depth = 1
