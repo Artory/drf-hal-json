31b203c824b99baa8122103440abe016357018cf
Johannes Staffans <johannes.staffans@artory.com>
2017-05-22 15:59:26 +0200

    Use custom serializer

--- tests/testproject/serializers.py
+++ tests/testproject/serializers.py
@@ -2,22 +2,20 @@ from drf_hal_json.serializers import HalModelSerializer
 from .models import TestResource, RelatedResource2, RelatedResource1
 
 
-class TestResourceSerializer(HalModelSerializer):
-    class Meta:
-        model = TestResource
-        fields = ('self', 'id', 'name', 'related_resource_1', 'related_resource_2')
-        depth = 1
-
-
 class RelatedResource1Serializer(HalModelSerializer):
     class Meta:
         model = RelatedResource1
-        fields = ('self', 'name', 'active')
-        depth = 1
+        fields = ('self', 'name')
 
 
 class RelatedResource2Serializer(HalModelSerializer):
     class Meta:
         model = RelatedResource2
-        fields = ('self', 'name', 'active', 'related_resources_1')
+
+
+class TestResourceSerializer(HalModelSerializer):
+    related_resource_1 = RelatedResource1Serializer(read_only=True)
+    class Meta:
+        model = TestResource
+        fields = ('self', 'id', 'name', 'related_resource_1', 'related_resource_2')
         depth = 1
