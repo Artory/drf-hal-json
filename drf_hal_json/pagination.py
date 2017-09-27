3df025d299ce23be1fc7d44d72fcd6589550b264
Julian Bez <julian.bez@artory.com>
2017-09-27 16:14:41 +0200

    Fix pagination

--- drf_hal_json/pagination.py
+++ drf_hal_json/pagination.py
@@ -10,33 +10,23 @@ from drf_hal_json import EMBEDDED_FIELD_NAME, LINKS_FIELD_NAME
 
 class HalPageNumberPagination(PageNumberPagination):
     def get_paginated_response(self, data):
-        result = OrderedDict()
-        links = OrderedDict()
-        links[api_settings.URL_FIELD_NAME] = {'href': self.request.build_absolute_uri()}
         if self.get_next_link():
-            links['next'] = {'href': self.get_next_link()}
+            data[LINKS_FIELD_NAME]['next'] = {'href': self.get_next_link()}
         if self.get_previous_link():
-            links['previous'] = {'href': self.get_previous_link()}
+            data[LINKS_FIELD_NAME]['previous'] = {'href': self.get_previous_link()}
         template_url = replace_query_param(self.request.build_absolute_uri(), self.page_query_param, '_PAGE_')
-        links['page'] = {
+        data[LINKS_FIELD_NAME]['page'] = {
             'href': template_url.replace('_PAGE_', '{?page}'),  # need this trick because of URL encoding
             'templated': True}
-        result[LINKS_FIELD_NAME] = links
-        result['count'] = self.page.paginator.count
-        result['page_size'] = self.get_page_size(self.request)
-        result[EMBEDDED_FIELD_NAME] = {'items': data}
-        return Response(result)
+        data['count'] = self.page.paginator.count
+        data['page_size'] = self.get_page_size(self.request)
+        return Response(data)
 
 
 class HalCursorPagination(CursorPagination):
     def get_paginated_response(self, data):
-        result = OrderedDict()
-        links = OrderedDict()
-        links[api_settings.URL_FIELD_NAME] = {'href': self.base_url}
         if self.get_next_link():
-            links['next'] = {'href': self.get_next_link()}
+            data[LINKS_FIELD_NAME]['next'] = {'href': self.get_next_link()}
         if self.get_previous_link():
-            links['previous'] = {'href': self.get_previous_link()}
-        result[LINKS_FIELD_NAME] = links
-        result[EMBEDDED_FIELD_NAME] = {'items': data}
-        return Response(result)
+            data[LINKS_FIELD_NAME]['previous'] = {'href': self.get_previous_link()}
+        return Response(data)
