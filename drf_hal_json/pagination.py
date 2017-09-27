30da0793ef4cf08afc8290b63402054ee662d579
Julian Bez <julian.bez@gmail.com>
2017-09-27 15:36:43 +0200

    Add page template for HalPageNumberPagination (#33)
    * Add page template for HalPageNumberPagination
    
    * Update pagination.py

--- drf_hal_json/pagination.py
+++ drf_hal_json/pagination.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from rest_framework.pagination import CursorPagination, PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.utils.urls import replace_query_param
 
 from drf_hal_json import EMBEDDED_FIELD_NAME, LINKS_FIELD_NAME
 
@@ -16,6 +17,10 @@ class HalPageNumberPagination(PageNumberPagination):
             links['next'] = {'href': self.get_next_link()}
         if self.get_previous_link():
             links['previous'] = {'href': self.get_previous_link()}
+        template_url = replace_query_param(self.request.build_absolute_uri(), self.page_query_param, '_PAGE_')
+        links['page'] = {
+            'href': template_url.replace('_PAGE_', '{?page}'),  # need this trick because of URL encoding
+            'templated': True}
         result[LINKS_FIELD_NAME] = links
         result['count'] = self.page.paginator.count
         result['page_size'] = self.get_page_size(self.request)
