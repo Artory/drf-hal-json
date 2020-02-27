895a94d0606357a888b0961c9eb3a9c827fe9f8f
Technology Admin <ambsw-technology@users.noreply.github.com>
2020-02-27 11:55:31 -0600

    Combine Imports

--- drf_hal_json/serializers.py
+++ drf_hal_json/serializers.py
@@ -4,10 +4,9 @@ from drf_hal_json import EMBEDDED_FIELD_NAME, LINKS_FIELD_NAME, URL_FIELD_NAME
 from drf_hal_json.fields import HalContributeToLinkField, HalHyperlinkedIdentityField, HalIncludeInLinksMixin
 from rest_framework.fields import empty
 from rest_framework.relations import HyperlinkedRelatedField, ManyRelatedField
-from rest_framework.serializers import BaseSerializer, HyperlinkedModelSerializer, ListSerializer
+from rest_framework.serializers import BaseSerializer, HyperlinkedModelSerializer, ListSerializer, Serializer
 from rest_framework.utils.field_mapping import get_nested_relation_kwargs
 from rest_framework.utils.serializer_helpers import ReturnDict
-from rest_framework.serializers import Serializer
 
 
 class HalListSerializer(ListSerializer):
